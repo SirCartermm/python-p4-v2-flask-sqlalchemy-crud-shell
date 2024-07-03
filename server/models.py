0d8520cf79b7d6225c70a0723a4809ff45a6e2de
SirCartermme <samuel.maina1@student.moringaschool.comm>
2024-07-03 15:20:27 +0300

    Modify model.py

--- server/models.py
+++ server/models.py
@@ -1,21 +1,9 @@
-from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import MetaData
+from app import db
 
-# contains definitions of tables and associated schema constructs
-metadata = MetaData()
-
-# create the Flask SQLAlchemy extension
-db = SQLAlchemy(metadata=metadata)
-
-# define a model class by inheriting from db.Model.
-
-
-class Pet(db.Model):
-    __tablename__ = 'pets'
-
+class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    species = db.Column(db.String)
+    name = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(100), nullable=False, unique=True)
 
     def __repr__(self):
-        return f'<Pet {self.id}, {self.name}, {self.species}>'
+        return f"User('{self.name}', '{self.email}')"
